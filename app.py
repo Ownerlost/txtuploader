9e5b6261ecd9d86fde040bb58e8ff31b7f0db38e
dev-Shailesh <159863419+Ownerlost@users.noreply.github.com>
2025-04-06 11:17:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Tech VJ'
+    return 'Hello from Զเधे Զเधे'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)  # Explicitly set port 8080
